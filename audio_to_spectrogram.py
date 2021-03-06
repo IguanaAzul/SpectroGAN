ae4d4e31681fbede630c8cb6d723b26cc78fa7b9
IguanaAzul <ramon.griffo98@gmail.com>
2021-03-06 19:01:06 -0300

    to implement gan

--- audio_to_spectrogram.py
+++ audio_to_spectrogram.py
@@ -13,34 +13,27 @@ def generate_spectrogram(x, sr, save_name):
     fig = plt.figure(figsize=(256, 256), dpi=32, frameon=False)
     ax = fig.add_axes([0, 0, 1, 1], frameon=False)
     ax.axis('off')
-    librosa.display.specshow(Xdb, sr=sr, cmap='grey')
+    librosa.display.specshow(Xdb, sr=sr, cmap='gray')
     plt.savefig(save_name, bbox_inches=0, pad_inches=0)
     plt.close()
     librosa.cache.clear()
 
-parser = ArgumentParser()
-parser.add_argument("-n", "--file_number", dest="file_number")
-args = parser.parse_args()
-
-file_number = int(args.file_number)
 audio_fpath = "./audios/"
 spectrograms_path = "./spectrograms/"
 audio_clips = os.listdir(audio_fpath)
 
-clip = audio_clips[file_number]
-#for idx, i in enumerate(audio_clips):
-#    print(idx, i)
-print(clip)
-
-audio_length = librosa.get_duration(filename=audio_fpath + clip)
-j=60
-while j < audio_length:
-    x, sr = librosa.load(audio_fpath + clip, offset=j-60, duration=60)
-    save_name = spectrograms_path + clip + str(j) + ".png"
-    generate_spectrogram(x, sr, save_name)
-    j += 60
-    if j >= audio_length:
-        j = audio_length
+for clip in audio_clips:
+    audio_length = librosa.get_duration(filename=audio_fpath + clip)
+    j=60
+    while j < audio_length:
         x, sr = librosa.load(audio_fpath + clip, offset=j-60, duration=60)
         save_name = spectrograms_path + clip + str(j) + ".png"
-        generate_spectrogram(x, sr, save_name)
+        if not os.path.exists(save_name):
+            generate_spectrogram(x, sr, save_name)
+        j += 60
+        if j >= audio_length:
+            j = audio_length
+            x, sr = librosa.load(audio_fpath + clip, offset=j-60, duration=60)
+            save_name = spectrograms_path + clip + str(j) + ".png"
+            if not os.path.exists(save_name):
+                generate_spectrogram(x, sr, save_name)
