b91516472d2dea021f37630de527eed8969f6761
IguanaAzul <ramon.griffo98@gmail.com>
2021-03-26 13:45:38 -0300

    Estudando o funcionamento dos espectrogramas. Criei notebook com as funções de conversão logarítmica para tornar viável a conversão dos nossos espectrogramas em imagens que podem ser lidas com o mínimo de perda de informação possível. Ainda falta resolver a perda de informação que acontece quando executamos stft

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 
 # Seta as variáveis
 seconds_per_file = 20
-image_size = 512
+image_size = 1024
 audio_fpath = "./audios/"
 spectrograms_path = "./spectrograms/"
 models_folder = "./models/"
@@ -21,32 +21,32 @@ beta1 = 0.5
 
 t0 = time.time()
 print("Gerando Espectrogramas")
-# generate_all_spectrograms(audio_fpath, spectrograms_path, seconds_per_file, image_size, replace=True)
+generate_all_spectrograms(audio_fpath, spectrograms_path, seconds_per_file, image_size)
 print(f"Tempo para gerar os espectrogramas: {time.time() - t0}")
 
-print("Treinando DCGAN")
-t0 = time.time()
-generator, discriminator = train_gan(
-        spectrograms_path, image_size, batch_size,
-        n_features_discriminator, n_features_generator,
-        z_vector, n_channels, num_epochs, beta1, lr,
-        save_every_epoch=True, seconds_per_file=20,
-        models_folder="./models/model/"
-)
-print(f"Tempo para treinar o modelo: {time.time() - t0}")
-
-save_model(
-        models_folder,
-        generator,
-        discriminator,
-        seconds_per_file,
-        image_size,
-        batch_size,
-        n_channels,
-        z_vector,
-        n_features_generator,
-        n_features_discriminator,
-        num_epochs,
-        lr,
-        beta1,
-)
+# print("Treinando DCGAN")
+# t0 = time.time()
+# generator, discriminator = train_gan(
+#         spectrograms_path, image_size, batch_size,
+#         n_features_discriminator, n_features_generator,
+#         z_vector, n_channels, num_epochs, beta1, lr,
+#         save_every_epoch=True, seconds_per_file=20,
+#         models_folder="./models/model/"
+# )
+# print(f"Tempo para treinar o modelo: {time.time() - t0}")
+#
+# save_model(
+#         models_folder,
+#         generator,
+#         discriminator,
+#         seconds_per_file,
+#         image_size,
+#         batch_size,
+#         n_channels,
+#         z_vector,
+#         n_features_generator,
+#         n_features_discriminator,
+#         num_epochs,
+#         lr,
+#         beta1,
+# )
